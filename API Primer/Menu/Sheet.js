a8607e5cf8cb76805b16c45c590061e2ae87489c
Gesus55555 <vlad16062001@gmail.com>
2020-09-02 10:17:20 +0300

    3

--- API Primer/Menu/Sheet.js
+++ API Primer/Menu/Sheet.js
@@ -1,9 +1,10 @@
+//For DataStructure
 class Sheet {
     constructor (UI, Name = String) {
         let SS = SpreadsheetApp.getActiveSpreadsheet();
         this.sheet = SS.getSheetByName(Name);
     }
-    ///Testing Git
+    
     GetDataRange() {
         return this.sheet.getRange()
     }
