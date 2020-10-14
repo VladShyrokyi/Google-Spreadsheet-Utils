ca81866ae5a50feca44dbfd469ac30c4ead02788
Gesus55555 <vlad16062001@gmail.com>
2020-10-14 21:01:17 +0300

    TEST

--- API Primer/Start.js
+++ API Primer/Start.js
@@ -1,10 +1,8 @@
 function onOpen() {
-  let SS = SpreadsheetApp.getActive();
-  let UI = SpreadsheetApp.getUi();
-  let Menu_SERP = new MenuSERP(UI, 'API Serpstat');
-  let Menu_GSC = new MenuGSC(UI, `GSC`);
+	let SS = SpreadsheetApp.getActive();
+	let UI = SpreadsheetApp.getUi();
+	let Menu_SERP = new MenuSERP(UI, "API Serpstat");
+	let Menu_GSC = new MenuGSC(UI, `GSC`);
 }
 
-function onEdit(e) {
-  
-}
+function onEdit(e) {}
