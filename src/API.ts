720f4a66dd5974002425fde20b54eb607f2e3881
Gesus55555 <vlad16062001@gmail.com>
2020-10-20 18:11:41 +0300

    Refactoring API/Data

--- src/API.ts
+++ src/API.ts
@@ -1,5 +1,5 @@
 interface IUrl {
-	[index: string]: string;
+  [index: string]: string;
 }
 /**
  * Create template query to API
@@ -12,36 +12,36 @@ interface IUrl {
  * @customfunction
  */
 function CreateQueryAPI(
-	Query: string,
-	Token: string,
-	ReportType: string,
-	Base: string,
-	SearchRegion: string,
-	Params?: Array<string>
+  Query: string,
+  Token: string,
+  ReportType: string,
+  Base: string,
+  SearchRegion: string,
+  Params?: Array<string>
 ): string {
-	let URL: IUrl = { Base, ReportType, Query, Token, SearchRegion };
-	for (let key in URL) {
-		let component: string = URL[key];
-		if (typeof component == `string`) {
-			if (component == Base) {
-				URL[key] = encodeURI(component);
-			} else {
-				URL[key] = encodeURIComponent(component);
-			}
-		}
-	}
-	let _URL = URL.Base;
-	_URL += `${URL.ReportType}`;
-	_URL += `?query=${URL.Query}`;
-	_URL += `&token=${Token}`;
-	_URL += `&se=${SearchRegion}`;
-	if (typeof Params == `object` && Params != null) {
-		for (let i = 0; i < Params.length; i++) {
-			_URL += `&${Params[i]}=${Params[i + 1]}`;
-			i++;
-		}
-	}
-	return _URL;
+  let URL: IUrl = { Base, ReportType, Query, Token, SearchRegion };
+  for (let key in URL) {
+    let component: string = URL[key];
+    if (typeof component == `string`) {
+      if (component == Base) {
+        URL[key] = encodeURI(component);
+      } else {
+        URL[key] = encodeURIComponent(component);
+      }
+    }
+  }
+  let _URL = URL.Base;
+  _URL += `${URL.ReportType}`;
+  _URL += `?query=${URL.Query}`;
+  _URL += `&token=${Token}`;
+  _URL += `&se=${SearchRegion}`;
+  if (typeof Params == `object` && Params != null) {
+    for (let i = 0; i < Params.length; i++) {
+      _URL += `&${Params[i]}=${Params[i + 1]}`;
+      i++;
+    }
+  }
+  return _URL;
 }
 /**
  * Create URL from template
@@ -50,20 +50,20 @@ function CreateQueryAPI(
  * @customfunction
  */
 function CreateUrlFromTemplate(URL: string, Query: string) {
-	if (Query == null || Query == `` || Query == `undefined`) {
-		return `No Query`;
-	}
-	let _Query = encodeURIComponent(Query);
-	let q = `?query=`;
-	let t = `&token=`;
-	let start: number = URL.indexOf(q);
-	let stop: number = URL.indexOf(t);
-	if (start != -1) {
-		let query = URL.slice(start + q.length, stop);
-		let _URL = URL.replace(query, _Query);
-		return _URL;
-	}
-	return `No Query`;
+  if (Query == null || Query == `` || Query == `undefined`) {
+    return `No Query`;
+  }
+  let _Query = encodeURIComponent(Query);
+  let q = `?query=`;
+  let t = `&token=`;
+  let start: number = URL.indexOf(q);
+  let stop: number = URL.indexOf(t);
+  if (start != -1) {
+    let query = URL.slice(start + q.length, stop);
+    let _URL = URL.replace(query, _Query);
+    return _URL;
+  }
+  return `No Query`;
 }
 /**
  * Get Data from url. Add it to the cache.
@@ -71,134 +71,140 @@ function CreateUrlFromTemplate(URL: string, Query: string) {
  * @customfunction
  */
 function FetchToAPI(URL: string): string {
-	if (URL == null || URL == `undefined` || URL == `` || URL == `No Query`) {
-		return URL;
-	}
-	let cache = CacheService.getDocumentCache();
-	let Key = URL.slice(0, 249);
-	if (cache?.get(Key) != null || cache?.get(Key) != undefined || cache?.get(Key) != ``) {
-		let content = UrlFetchApp.fetch(URL).getContentText();
-		cache?.remove(Key);
-		cache?.put(Key, content);
-	}	else if (cache == null) {
-		return `Not cache`;
-	}
-	return Key;
+  if (URL == null || URL == `undefined` || URL == `` || URL == `No Query`) {
+    return URL;
+  }
+  let cache = CacheService.getDocumentCache();
+  let Key = URL.slice(0, 249);
+  if (
+    cache?.get(Key) != null ||
+    cache?.get(Key) != undefined ||
+    cache?.get(Key) != ``
+  ) {
+    let content = UrlFetchApp.fetch(URL).getContentText();
+    cache?.remove(Key);
+    cache?.put(Key, content);
+  } else if (cache == null) {
+    return `Not cache`;
+  }
+  return Key;
 }
 /**
  * Outputting data along a path from the cache.
  * Options: CountMax - maximum rows to output
  * @param Key Cache key entry
  * @param Path String Path/to data/or params
- * @param Options Example: {"HideParam", "CountMax = 10", "JSON"}
+ * @param Options Example: "HideParam", "CountMax = 10", "JSON"
  * @customfunction
  */
 function GiveFromCache(Key: string, Path: string, ...Options: string[]) {
-	let cache = CacheService.getDocumentCache();
-	if (cache == null) {
-		return `No cache`;
-	}
-	let content = cache.get(Key);
-	if (content == null) {
-		return `-`;
-	}
-	let data: element = JSON.parse(content);
-	let _Options: OptionsValid = {};
-	if (Options != null) {
-		_Options = new OptionsValid(...Options);
-	}
-	if (Path != null) {
-		let PathArr = Path.split(`/`);
-		for (let i = 0; i < PathArr.length; i++) {
-			data = ValidateElement(data, PathArr[i]);
-		}
-	} else {
-		return `No Path`;
-	}
-	return CreateOutput(data, _Options);
+  let cache = CacheService.getDocumentCache();
+  if (cache == null) {
+    return `No cache`;
+  }
+  let content = cache.get(Key);
+  if (content == null) {
+    return `-`;
+  }
+  let data: element = JSON.parse(content);
+  let _Options: OptionsValid = {};
+  if (Options != null) {
+    _Options = new OptionsValid(...Options);
+  }
+  if (Path != null) {
+    let PathArr = Path.split(`/`);
+    for (let i = 0; i < PathArr.length; i++) {
+      data = ValidateElement(data, PathArr[i]);
+    }
+  } else {
+    return `No Path`;
+  }
+  return CreateOutput(data, _Options);
 }
-type element = elementArray | elementObj | string;
+
+//Element from JSON//////////////////////
+type element = elementArray | elementObj | string | number;
 interface elementObj {
-	[key: string]: element;
+  [key: string]: element;
 }
 interface elementArray extends Array<element> {
-	[index: number]: element;
+  [index: number]: element;
 }
+//////////////////////////////////////////
+
 function ValidateElement(e: element, Path: string): element {
-	if (Array.isArray(e)) {
-		let arr: elementArray = [];
-		for (let i = 0; i < e.length; i++) {
-			let _e = e[i];
-			let result = ValidateElement(_e, Path);
-			arr.push(result);
-		}
-		return arr;
-	} else if (typeof e == `object`) {
-		return ValidateObject(e, Path);
-	} else {
-		return e;
-	}
+  if (Array.isArray(e)) {
+    let arr: elementArray = [];
+    for (let i = 0; i < e.length; i++) {
+      let _e = e[i];
+      let result = ValidateElement(_e, Path);
+      arr.push(result);
+    }
+    return arr;
+  } else if (typeof e == `object`) {
+    return ValidateObject(e, Path);
+  } else {
+    return e;
+  }
 }
 function ValidateObject(e: elementObj, Path: string): element {
-	for (let property in e) {
-		if (property == Path) {
-			return e[property];
-		}
-	}
-	return `Element empty`;
+  for (let property in e) {
+    if (property == Path) {
+      return e[property];
+    }
+  }
+  return `Element empty`;
 }
 function CreateOutput(data: element, Options: OptionsValid) {
-	if (typeof data == `string`) {
-		return data.toString();
-	} else if (typeof data == `number`) {
-		return parseInt(data);
-	} else if (Array.isArray(data)) {
-		let DoubleArr: Array<string[] | string> = [];
-		for (let y = 0; y < data.length; y++) {
-			let Value: element = data[y];
-			if (typeof Value == "string" || typeof Value == `number`) {
-				DoubleArr[y] = Value;
-			} else if (Array.isArray(Value)) {
-				let Arr: string[] = [];
-				for (let x = 0; x < Value.length; x++) {
-					let element = Value[x];
-					if (typeof element == `object`) {
-						Arr[x] = JSON.stringify(element);
-					} else {
-						Arr[x] = element;
-					}
-				}
-				DoubleArr.push(Arr);
-			} else {
-				let Arr: string[] = [];
-				for (let x in Value) {
-					Arr.push(JSON.stringify(Value[x]));
-				}
-				DoubleArr.push(Arr);
-			}
-			if (
-				Options != null &&
-				Options[`isCountMax`] == true &&
-				Options[`CountMax`] == y
-			) {
-				break;
-			}
-		}
-		return DoubleArr;
-	} else {
-		return JSON.stringify(data);
-	}
+  if (typeof data == `string`) {
+    //Return string
+    return data;
+  } else if (typeof data == `number`) {
+    //Return number
+    return data;
+  } else if (Options?.[`JSON`]) {
+    //Check Options
+    return JSON.stringify(data);
+  } else if (Array.isArray(data)) {
+    //Create Result Array
+    let DoubleArr: Array<Array<string | number> | string | number> = [];
+
+    //Іterating over an array
+    data.forEach((Value, y) => {
+      if (Options?.[`isCountMax`] == true && Options[`CountMax`] == y) return; //Check options
+      if (typeof Value == 'string' || typeof Value == `number`)
+        DoubleArr[y] = Value;
+      else if (Array.isArray(Value))
+        Value.forEach((e, i) =>
+          typeof e == `object`
+            ? DoubleArr.push([JSON.stringify(e)])
+            : DoubleArr.push([e])
+        );
+      else DoubleArr.push(JSON.stringify(Object.values(Value)));
+    });
+
+    //Return result Array
+    return DoubleArr;
+  } else {
+    //Return object as string
+    return Object.values(data);
+  }
 }
 interface IOptions {
-	[key: string]: Boolean | number;
+  [key: string]: Boolean | number;
 }
 class OptionsValid implements IOptions {
-	[key: string]: number | Boolean;
-	constructor(...Options: string[]) {
-		Options.find((e: string | string[]) => {
-			if (typeof e == `string` && e.includes(` = `)) e = e.split(` = `);
-			this[`is${e[0]}`] = true;
-			this[e[0]] = parseInt(e[1]) - 1;
-		}, this);
-	}
+  [key: string]: number | Boolean;
+  constructor(...Options: string[]) {
+    Options.find((e: string) => {
+      let arr = [];
+      if (e.includes(` = `)) {
+        arr = e.split(` = `);
+        this[`is${arr[0]}`] = true;
+        this[arr[0]] = parseInt(e[1]) - 1;
+      } else {
+        this[`is${e}`] = true;
+      }
+    }, this);
+  }
 }
--- src/Data.ts
+++ src/Data.ts
@@ -1,125 +1,125 @@
 interface IData {
-	readonly sheet: GoogleAppsScript.Spreadsheet.Sheet;
-	readonly _range: GoogleAppsScript.Spreadsheet.Range;
-	readonly Rows: number;
-	readonly Columns: number;
-	readonly data: any[][];
+  readonly sheet: GoogleAppsScript.Spreadsheet.Sheet;
+  readonly _range: GoogleAppsScript.Spreadsheet.Range;
+  readonly Rows: number;
+  readonly Columns: number;
+  readonly data: any[][];
 }
 class Data implements IData {
-	sheet: GoogleAppsScript.Spreadsheet.Sheet;
-	_range: GoogleAppsScript.Spreadsheet.Range;
-	Rows: number;
-	Columns: number;
-	data: any[][];
-	constructor(range: GoogleAppsScript.Spreadsheet.Range) {
-		this._range = range;
-		this.Rows = this._range.getNumRows();
-		this.Columns = this._range.getNumColumns();
-		range;
-		this.data = this._range.getValues();
-		this.sheet = this._range.getSheet();
-	}
-	Sorting(StartRow: number, SortingColumn: number) {
-		let checkValue: string | number = this._range
-			.getCell(StartRow, SortingColumn)
-			.getValue();
-		let Arr = [];
-		for (let i = StartRow; i <= this.Rows; i++) {
-			let Value = this.data[i - 1][SortingColumn];
-			if (Value == checkValue) {
-				Array.isArray(Arr[Arr.length - 1])
-					? Arr[Arr.length - 1].push(i)
-					: Arr[0].push(i);
-			} else {
-				Arr.push([i]);
-			}
-			checkValue = Value;
-		}
-		// return CustomUI.showMessageBox(`Array`, JSON.stringify(Arr));
-		for (let i = 0; i < Arr.length; i++) {
-			let Group = Arr[i];
-			let Start = Group[1];
-			let End = Group.length - 1;
-			if (Start == null) Start = Group[0] + 1;
-			if (End == 0) End = 1;
-			try {
-				this.sheet.getRowGroup(Start, 1)?.collapse(); //if Group has been
-			} catch (error) {
-				this.sheet
-					.getRange(Start, SortingColumn, End)
-					.shiftRowGroupDepth(1)
-					.collapseGroups();
-			}
-		}
-	}
-	// directed 1 - column+: 0 - row+
-	Copy(directed: number = 0) {
-		let rangeNew: GoogleAppsScript.Spreadsheet.Range;
-		if (directed == 1) rangeNew = this._range.offset(0, this.Columns);
-		else if (directed == 0) rangeNew = this._range.offset(this.Rows, 0);
-		else rangeNew = this._range;
-		this._range.copyTo(rangeNew);
-	}
+  sheet: GoogleAppsScript.Spreadsheet.Sheet;
+  _range: GoogleAppsScript.Spreadsheet.Range;
+  Rows: number;
+  Columns: number;
+  data: any[][];
+  constructor(range: GoogleAppsScript.Spreadsheet.Range) {
+    this._range = range;
+    this.Rows = this._range.getNumRows();
+    this.Columns = this._range.getNumColumns();
+    range;
+    this.data = this._range.getValues();
+    this.sheet = this._range.getSheet();
+  }
+  Sorting(StartRow: number, SortingColumn: number) {
+    let checkValue: string | number = this._range
+      .getCell(StartRow, SortingColumn)
+      .getValue();
+    let Arr = [];
+    for (let i = StartRow; i <= this.Rows; i++) {
+      let Value = this.data[i - 1][SortingColumn];
+      if (Value == checkValue) {
+        Array.isArray(Arr[Arr.length - 1])
+          ? Arr[Arr.length - 1].push(i)
+          : Arr[0].push(i);
+      } else {
+        Arr.push([i]);
+      }
+      checkValue = Value;
+    }
+    // return CustomUI.showMessageBox(`Array`, JSON.stringify(Arr));
+    for (let i = 0; i < Arr.length; i++) {
+      let Group = Arr[i];
+      let Start = Group[1];
+      let End = Group.length - 1;
+      if (Start == null) Start = Group[0] + 1;
+      if (End == 0) End = 1;
+      try {
+        this.sheet.getRowGroup(Start, 1)?.collapse(); //if Group has been
+      } catch (error) {
+        this.sheet
+          .getRange(Start, SortingColumn, End)
+          .shiftRowGroupDepth(1)
+          .collapseGroups();
+      }
+    }
+  }
+  // directed 1 - column+: 0 - row+
+  Copy(directed: number = 0) {
+    let rangeNew: GoogleAppsScript.Spreadsheet.Range;
+    if (directed == 1) rangeNew = this._range.offset(0, this.Columns);
+    else if (directed == 0) rangeNew = this._range.offset(this.Rows, 0);
+    else rangeNew = this._range;
+    this._range.copyTo(rangeNew);
+  }
 }
 class ActiveDate extends Data {
-	constructor(readonly sheet: GoogleAppsScript.Spreadsheet.Sheet) {
-		super(sheet.getActiveRange() || sheet.getRange(1, 1));
-	}
+  constructor(readonly sheet: GoogleAppsScript.Spreadsheet.Sheet) {
+    super(sheet.getActiveRange() || sheet.getRange(1, 1));
+  }
 }
 class TemplateData extends Data {
-	// _header: Array<string | number>;
-	constructor(
-		readonly sheet: GoogleAppsScript.Spreadsheet.Sheet,
-		RowStart: number,
-		ColumnStart: number,
-		height: number,
-		width: number
-	) {
-		super(sheet.getRange(RowStart, ColumnStart, height, width));
-		this.header = sheet.getRange(RowStart, width).getValues()[0];
-	}
-	set header(Values: Array<string | number>) {
-		if (Array.isArray(Values)) this.header = Values;
-		else {
-			this.header = [];
-			this.header[0] = Values;
-		}
-	}
-	get header() {
-		return this.header;
-	}
+  // _header: Array<string | number>;
+  constructor(
+    readonly sheet: GoogleAppsScript.Spreadsheet.Sheet,
+    RowStart: number,
+    ColumnStart: number,
+    height: number,
+    width: number
+  ) {
+    super(sheet.getRange(RowStart, ColumnStart, height, width));
+    this.header = sheet.getRange(RowStart, width).getValues()[0];
+  }
+  set header(Values: Array<string | number>) {
+    if (Array.isArray(Values)) this.header = Values;
+    else {
+      this.header = [];
+      this.header[0] = Values;
+    }
+  }
+  get header() {
+    return this.header;
+  }
 }
 function MarkingQuery() {
-	let _token = `84a70800e984ef048ae0e352d2e093ed`;
-	let _reportType = ``;
-	let _base = ``;
-	let _searchRegion = ``;
-	let _result = `result`;
-	let _domain = `Domain`;
-	let _keywords = `Keywords`;
-	let sheet = SpreadsheetApp.getActiveSheet();
-	let template = new TemplateData(sheet, 1, 1, 5, 2);
-	let query: string[] = [];
-	template.header.find((v, i) => {
-		if (v == _domain && v == _keywords) {
-			template.data.forEach((y) => query.push(y[i]));
-		}
-	});
-	let URL = query.map((e) =>
-		CreateQueryAPI(e, _token, _reportType, _base, _searchRegion)
-	);
-	let Keys = URL.map((e) => FetchToAPI(e));
-	let Values: any[][] = [];
-	let x = Values[0];
-	Keys.forEach((e, i) => {
-		let val = GiveFromCache(e, _result);
-		Array.isArray(val) ? (Values[i] = val) : (Values[i][0] = val);
-	});
-	let range = sheet.getRange(1, 3, 5, 2);
-	range.setValues(Values);
-}
-// class MarkingQuery {
-// 	constructor(template: TemplateData) {
+  let _token = `84a70800e984ef048ae0e352d2e093ed`;
+  let _reportType = [`keyword_info`, `domain_info`];
+  let _base = `http://api.serpstat.com/v3/`;
+  let _searchRegion = `g_ua`;
+  let _result = `result`;
+  let _domain = `Domain`;
+  let _keywords = `Keywords`;
+  let sheet = SpreadsheetApp.getActiveSheet();
+  let template = new TemplateData(sheet, 1, 1, 5, 2);
+  let query: string[][] = [];
 
-// 	}
-// }
+  template.header.find((v, i) => {
+    if (v == _domain)
+      template.data.forEach((y) => (i != 0 ? query[0].push(y[i]) : ``));
+    else if (v == _keywords)
+      template.data.forEach((y) => (i != 0 ? query[1].push(y[i]) : ``));
+  });
+
+  let URLs = query.map((e, i) =>
+    e.map((q) =>
+      CreateQueryAPI(q, _token, _reportType[i], _base, _searchRegion)
+    )
+  );
+  let Keys = URLs.map((e) => e.map((q) => FetchToAPI(q)));
+  let Values: any[][] = [];
+  let x = Values[0];
+  Keys.forEach((e, i) => {
+    let val = GiveFromCache(e, _result);
+    Array.isArray(val) ? (Values[i] = val) : (Values[i][0] = val);
+  });
+  let range = sheet.getRange(1, 3, 5, 2);
+  range.setValues(Values);
+}
