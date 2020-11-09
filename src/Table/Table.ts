2c70005f3eacddb60cdf750a4d90c3ce5c265cae
Gesus55555 <vlad16062001@gmail.com>
2020-11-09 10:55:06 +0200

    Fix bug: Table/Class

--- src/Table/Table.ts
+++ src/Table/Table.ts
@@ -67,3 +67,122 @@ class ActiveTable extends Table {
     super(sheet.getActiveRange() || sheet.getRange(1, 1));
   }
 }
+
+interface TemplateCreator {
+  sheet: GoogleAppsScript.Spreadsheet.Sheet;
+  RowStart: number;
+  ColumnStart: number;
+  height: number;
+  width: number;
+}
+
+class TemplateTable extends Table {
+  private _header: element[] = [];
+  private _values: element[][] = [];
+
+  public getHeader(): element[] {
+    return this._header;
+  }
+  public set header(value: element[] | element) {
+    this._header = Array.isArray(value) ? value : [value];
+  }
+  public getValues(): element[][] {
+    return this._values;
+  }
+  public set values(value: Array<element | element[]> | element) {
+    this._values = Array.isArray(value)
+      ? value.map((r) => (Array.isArray(r) ? r : [r]))
+      : [[value]];
+  }
+
+  constructor(Options: TemplateCreator) {
+    super(
+      Options.sheet.getRange(
+        Options.RowStart,
+        Options.ColumnStart,
+        Options.height,
+        Options.width
+      )
+    );
+    this.header = this.data.getHeader();
+    if (Options.height > 1) this.values = this.data.sliceData(1);
+  }
+}
+
+class MarkingTable extends TemplateTable {
+  _base: string = `http://api.serpstat.com/v3/`;
+  _reportType: string[] = [`domain_keywords`, `keywords`];
+  _query: element[][] = [[], []];
+  _token: string = `84a70800e984ef048ae0e352d2e093ed`;
+  _searchRegion: string = `g_ua`;
+
+  Keys: string[];
+  constructor(sheet: GoogleAppsScript.Spreadsheet.Sheet) {
+    super({ sheet, RowStart: 1, ColumnStart: 1, height: 5, width: 2 });
+    let domain = `Domain`;
+    let keywords = `Keywords`;
+    this.getHeader().find(
+      (headerName, index) =>
+        (headerName == domain &&
+          this.getValues().forEach((r) => this._query[0].push(r[index]))) ||
+        (headerName == keywords &&
+          this.getValues().forEach((r) => this._query[1].push(r[index])))
+    );
+    this.Keys = [];
+  }
+  FetchToAPI(): MarkingTable {
+    let URLs: API[] = [];
+    this._query.forEach((e, i) =>
+      e.forEach((q) =>
+        URLs.push(
+          new API(
+            new URI(
+              CreateAPI(
+                this._base,
+                this._reportType[i],
+                q.toString(),
+                this._token,
+                this._searchRegion
+              )
+            )
+          )
+        )
+      )
+    );
+    this.Keys = URLs.map((str) => str.FetchQuery());
+    return this;
+  }
+  GiveValuesFromAPI(): element[][] {
+    let result = `result/hits`;
+    let resultArr: element[][] = [];
+    this.Keys.forEach((e) => {
+      let Data = GiveFromCache(e, result);
+      Array.isArray(Data)
+        ? Data.forEach((Row) =>
+            resultArr.push(
+              Array.isArray(Row)
+                ? Row //.map((Value) => Value)
+                : [Row]
+            )
+          )
+        : resultArr.push([Data]);
+    });
+    return resultArr;
+  }
+}
+
+function MarkingQuery() {
+  let sheet = SpreadsheetApp.getActiveSheet();
+  let template = new MarkingTable(sheet);
+  let Values = template.FetchToAPI().GiveValuesFromAPI();
+  let lengthMax = Values.reduce(
+    (max, e, i, arr) => (max > e.length ? max : e.length),
+    0
+  );
+  let CorrectedValues: element[][] = Values.map((arr) => {
+    arr.length = lengthMax;
+    return arr;
+  });
+  let range = sheet.getRange(1, 3, CorrectedValues.length, lengthMax);
+  range.setValues(Values);
+}
